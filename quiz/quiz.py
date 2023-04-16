d9f951001d71f7fbd94cb7e2cc4adaabe581df11
Megan Miller <megan.miller@smoldev.dev>
2023-04-16 19:01:12 -0400

    Quiz Answers.

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,9 +1,21 @@
 # What is the data type in the following variable?
-my_var = {"name": "John", "age": 23} # answer here
+my_var = {"name": "John", "age": 23} # dictionary - key: value pairs
 
 # Write a loop that prints out numbers from 1 to 10.
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for num in digits:
+    print(num)
 
 # Write a function that returns a sentence using a name given to it.
 # Remember to call the function so that it prints in the console!
+def greeting(name):
+    return f"Hello, {name}!"
+
+print(greeting("Sol"))
 
 # BONUS: What are the four different types of collections in python?
+# Lists = []
+# Tuples = ()
+# Sets = {}
+# Dictionaries = {key: value}
